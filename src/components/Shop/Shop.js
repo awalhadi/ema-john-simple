942f55efbca7ca647a55c3e2bb895e16f1506de0
Md A Awal Hadi <awalhadi5@gmail.com>
2021-04-09 06:29:18 +0600

    order summery fixed for all page with same data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import fakeData from "../../fakeData";
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import "./Shop.css";
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProduct] = useState(first10);
     const [cart, setCart] = useState([]);
     
+    // load data fro local storage
+    useEffect( () => {
+        const saveCart = getDatabaseCart();
+        const productKeys = Object.keys(saveCart);
+        const previousCart = productKeys.map(exsistingKey => {
+            const product = fakeData.find(pd => pd.key === exsistingKey);
+            product.quantity = saveCart[exsistingKey];
+            return product;
+        })
+        console.log(previousCart);
+        setCart(previousCart);
+    }, [])
     const handleClick = (product) => {
         const toBeAddedKey = product.key;
         const sameProduct = cart.find(pd => pd.key === toBeAddedKey);
